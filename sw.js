038b3db5133348b16a1a737f55b739bd8bbbb513
Zubair <zubairbhuian@gmail.com>
2022-02-06 13:44:28 +0600

    up:2.6.4

--- sw.js
+++ sw.js
@@ -1,6 +1,27 @@
+const cachesFolder = 'firest-catch';
+const assets = [
+    '/',
+    '/index.html',
+    '/js/app.js',
+    '/js/ui.js',
+    '/js/materialize.min.js',
+    '/css/styles.css',
+    '/css/materialize.min.css',
+    '/img/dish.png',
+    'https://fonts.googleapis.com/icon?family=Material+Icons',
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+  ];
+
 // install
 self.addEventListener('install', e =>{
-    console.log('service worker installed');
+    // console.log('service worker installed');
+    e.waitUntil(
+        caches.open(cachesFolder).then( cache =>{
+            console.log('caching sell assets');
+            cache.addAll(assets);
+        })
+    )
+
 });
 // active
 self.addEventListener('activate', e =>{
@@ -8,5 +29,5 @@ self.addEventListener('activate', e =>{
 });
 // fetch
 self.addEventListener('fetch', e =>{
-    console.log('fetch');
+    console.log('fetching', e)
 })
