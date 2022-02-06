1b0003927591d1226ce2b33572625f453c02b300
Zubair <zubairbhuian@gmail.com>
2022-02-06 15:04:55 +0600

    up:2.6.5

--- sw.js
+++ sw.js
@@ -9,7 +9,8 @@ const assets = [
     '/css/materialize.min.css',
     '/img/dish.png',
     'https://fonts.googleapis.com/icon?family=Material+Icons',
-    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
+    'https://fonts.gstatic.com/s/materialicons/v47/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2',
+    'https://fonts.gstatic.com/s/materialicons/v121/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2'
   ];
 
 // install
@@ -29,5 +30,11 @@ self.addEventListener('activate', e =>{
 });
 // fetch
 self.addEventListener('fetch', e =>{
-    console.log('fetching', e)
+    // console.log('fetching', e)
+    e.respondWith(
+        caches.match(e.request)
+        .then( catchRes =>{
+            return catchRes || fetch(e.request);
+        })
+    )
 })
