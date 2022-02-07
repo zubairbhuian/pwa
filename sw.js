e6b979279f3fe4cc16b225d818212e7bb64e785e
Zubair <zubairbhuian@gmail.com>
2022-02-07 16:37:17 +0600

    2.7.6

--- sw.js
+++ sw.js
@@ -1,4 +1,5 @@
-const staticcacheName = "cache-v2";
+const staticCacheName = "cache-v1";
+const dynamicCacheName = 'cache-v2';
 
 const assets = [
   "/",
@@ -18,26 +19,43 @@ const assets = [
   "https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i",
   "https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css",
   'https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js',
-  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css'
+  'https://cdn.jsdelivr.net/npm/bootstrap-icons@1.8.0/font/bootstrap-icons.css',
+  '/fallback.html'
 ];
+
+
+// cache size limit function
+const limitCacheSize = (name, size) => {
+  caches.open(name).then(cache => {
+    cache.keys().then(keys => {
+      if(keys.length > size){
+        cache.delete(keys[0]).then(limitCacheSize(name, size));
+      }
+    });
+  });
+};
+
+
+
+
 // install
 self.addEventListener("install", (e) => {
   // console.log('Install Successful');
   e.waitUntil(
-    caches.open(staticcacheName).then((catche) => {
-      catche.addAll(assets);
+    caches.open(staticCacheName).then((cache) => {
+      cache.addAll(assets);
     })
   );
 });
-// demo ssdsfsfd sfggdhkh khjk uy u
+// demo sdfdsfffd sfggdkh kk uy u
 // Activate
 self.addEventListener("activate", (e) => {
-  console.log("Activate Successful", e);
-  evt.waitUntil(
+  // console.log("Activate Successful", e);
+  e.waitUntil(
     caches.keys().then(keys => {
       //console.log(keys);
       return Promise.all(keys
-        .filter(key => key !== staticCacheName)
+        .filter(key => key !== staticCacheName && key !== dynamicCacheName)
         .map(key => caches.delete(key))
       );
     })
@@ -45,12 +63,22 @@ self.addEventListener("activate", (e) => {
 });
 
 // Fetch 
-self.addEventListener("fetch", (e) => {
-//   console.log("Fetch Successful");
-e.respondWith(
+self.addEventListener('fetch', e => {
+  //console.log('fetch event', e);
+  e.respondWith(
     caches.match(e.request).then(cacheRes => {
-      return cacheRes || fetch(e.request);
+      return cacheRes || fetch(e.request).then(fetchRes => {
+        return caches.open(dynamicCacheName).then(cache => {
+          cache.put(e.request.url, fetchRes.clone());
+          // check cached items size
+          limitCacheSize(dynamicCacheName, 5);
+          return fetchRes;
+        })
+      });
+    }).catch(() => {
+      if(e.request.url.indexOf('.html') > -1){
+        return caches.match('/fallback.html');
+      } 
     })
   );
 });
-// end bhljk
